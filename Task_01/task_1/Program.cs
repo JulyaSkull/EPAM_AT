4b3c13428813f1d0053b578c3205f0fcaad5c3e5
Julya <jula.popova99@gmail.com>
2020-11-20 02:48:08 +0300

    C# Basics

---
+++ Task_01/task_1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = new int[] { 16, 14, 15, 18, 21, 34, 10, 7 };
+
+            Surveillance_Of_Elements(array);
+
+            Console.ReadKey();
+            int temp;
+            for (int i = 0; i < array.Length - 1; i++)
+            {
+                for (int j = i + 1; j < array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+
+                        temp = array[i];
+                        array[i] = array[j];
+                        array[j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Sort array");
+
+            Surveillance_Of_Elements(array);
+
+            Console.ReadKey();
+        }
+
+            static void Surveillance_Of_Elements(int[]array)
+            {
+            int count = 0;
+                foreach (int x in array)
+                {
+                    count++;
+
+                    Console.WriteLine($"#{count} : {x} \t");
+                }
+            }
+        
+    }
+}
+
---
+++ Task_01/task_2/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool success;
+            float first_number;
+            float second_number;
+            do
+            {
+                Console.WriteLine("Input float first number x");
+                success = float.TryParse(Console.ReadLine(), out first_number);
+                if (!success)
+                {
+                    Console.WriteLine("Incorrect value");
+                }
+            } while (!success);
+
+            do
+            {
+                Console.WriteLine("Input float second number y");
+                success = float.TryParse(Console.ReadLine(), out second_number);
+                if (!success)
+                {
+                    Console.WriteLine("Incorrect value");
+                }
+            } while (!success);
+            success = false;
+            while (!success)
+            {
+                Console.WriteLine("Select the required action: +, -, /, *");
+                string act = Console.ReadLine();
+                switch (act)
+                {
+                    case "+":
+                        float sum = first_number + second_number;
+                        Console.WriteLine($"x + y = {sum}");
+                        success = true;
+                        break;
+                    case "-":
+                        float sub = first_number - second_number;
+                        Console.WriteLine($"x - y = {sub}");
+                        success = true;
+                        break;
+                    case "*":
+                        float mult = first_number * second_number;
+                        Console.WriteLine($"x * y = {mult}");
+                        success = true;
+                        break;
+                    case "/":
+                        if (second_number == 0)
+                        {
+                            Console.WriteLine("Divide by 0. You can't divide this.");
+                            break;
+                        }
+                        float div = first_number / second_number;
+                        Console.WriteLine($"x / y = {div}");
+                        success = true;
+                        break;
+                    default:
+                        Console.WriteLine("Wrong operation");
+                        break;
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
